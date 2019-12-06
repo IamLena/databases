b79f086222b0496120c2ad423267c1aef17a6dc4
IamLena <34005469+IamLena@users.noreply.github.com>
2019-12-06 22:54:06 +0300

    funcs and two procedures

--- funcs.sql
+++ funcs.sql
@@ -11,7 +11,7 @@ begin
 	select @filename = Device.nick + '-'
 	+ Cast(Size.scale as varchar) + 'x-'
 	+ Os.nick + '-' + Master.short_cont + '-'
-	+Langs.lang_code + '-' + Langs.country_code + '.psd'
+	+Langs.lang_code + '-' + Langs.country_code + '-by_' + Designer.nick + '.psd'
 
 	from OsDeviceMaster join Device
 	on OsDeviceMaster.id_device = Device.id
@@ -23,6 +23,8 @@ begin
 	on Size.id_size = Master.size
 	join Langs
 	on Langs.id = Master.lang_id
+	join Designer
+	on Master.designer = Designer.id
 
 	where Master.id = @master_id;
 	return @filename;
@@ -30,14 +32,134 @@ end;
 go
 
 select dbo.get_psd(5) as new_psd
+go
 --drop function dbo.get_psd;
 
 --2) Подставляемую табличную функцию
+--все скрины и их масштаб сделанный на мастере определенного id
+create function showScreensOfMaster
+(@master_id int)
+returns table
+as
+return (
+	select ScreenShot.*, Size.scale
+	from ScreenShot join Size
+	on ScreenShot.size = Size.id_size
+	where id_master = @master_id
+);
+go
+
+select * from showScreensOfMaster(20)
+go
+
 --3) Многооператорную табличную функцию
+create function getsize (@object varchar(10), @object_id int)
+returns @sizetable table (width int, height int)
+as
+begin
+	if (@object = 'device')
+	begin
+		insert @sizetable (width, height)
+		select width, height
+		from Device join Size
+		on Device.size = Size.id_size
+		where Device.id = @object_id
+	end
+	else if (@object = 'master')
+	begin
+		insert @sizetable (width, height)
+		select width, height
+		from Master join Size
+		on Master.size = Size.id_size
+		where Master.id = @object_id
+	end
+	else if (@object = 'screenshot')
+	begin
+		insert @sizetable (width, height)
+		select width, height
+		from ScreenShot join Size
+		on ScreenShot.size = Size.id_size
+		where ScreenShot.id = @object_id
+	end
+	return
+end;
+go
+
+drop function dbo.getsize;
+
+select * from getsize('device', 2)
+select * from getsize('master', 2)
+select * from getsize('screenshot', 2)
+go
+
 --4) Рекурсивную функцию или функцию с рекурсивным ОТВ B
+create function show_origins
+(@id_master int)
+returns @OriginPath table
+	(id int,
+	lang_id int,
+	short_cont nvarchar(20),
+	mod_time datetime,
+	designer int,
+	psd nvarchar(70),
+	size int,
+	origin_master_id int,
+	level int)
+as
+begin
+	with otb_masters(id, lang_id, short_cont, mod_time, designer, psd, size, origin_master_id, level)
+	as
+	(	select id, lang_id, short_cont, mod_time, designer, psd, size,  origin_master_id, 0 as level
+		from Master
+		where id = @id_master
+		union all
+		select Master.id, Master.lang_id, Master.short_cont, Master.mod_time, Master.designer, Master.psd, Master.size, Master.origin_master_id, level + 1
+		from otb_masters join Master
+		on otb_masters.origin_master_id = Master.id
+	)
+	insert @OriginPath (id, lang_id, short_cont, mod_time, designer, psd, size, origin_master_id, level)
+	select *
+	from otb_masters
+	return
+end;
+go
+
+--drop function show_origins;
+select * from dbo.show_origins(220);
+go
 
 --1) Хранимую процедуру без параметров или с параметрами
---2) Рекурсивную хранимую процедуру или хранимую процедур с рекурсивным ОТВ
+create procedure update_psd(@master_id int)
+as
+begin
+	update Master
+	set psd = dbo.get_psd(@master_id)
+	where Master.id = @master_id
+end
+go
+execute update_psd 4
+go
+
+--2) Рекурсивную хранимую процедуру или хранимую процедуру с рекурсивным ОТВ
+create procedure updateAllPsd (@id int, @maxid int)
+as
+begin
+	IF (@id <= @maxid)
+	begin
+		update Master
+		set psd = dbo.get_psd(@id)
+		where Master.id = @id
+
+		set @id = @id + 1
+		execute updateAllPsd @id, @maxid
+	end
+	return
+end
+go
+
+execute updateAllPsd 1, 5;
+go
+
 --3) Хранимую процедуру с курсором
 --4) Хранимую процедуру доступа к метаданным
 
