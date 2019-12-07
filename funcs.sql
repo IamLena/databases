36bcc9c990cdb41a5ec263b130b806426360665e
IamLena <34005469+IamLena@users.noreply.github.com>
2019-12-07 12:32:33 +0300

    all

--- funcs.sql
+++ funcs.sql
@@ -161,7 +161,75 @@ execute updateAllPsd 1, 5;
 go
 
 --3) Хранимую процедуру с курсором
+create procedure updatePSDcursor
+as
+begin
+DECLARE @curid int
+DECLARE master_cursor CURSOR FOR
+SELECT id FROM Master
+
+OPEN master_cursor
+FETCH NEXT FROM master_cursor INTO @curid
+WHILE @@FETCH_STATUS = 0
+BEGIN
+   update Master
+	set psd = dbo.get_psd(@curid)
+	where Master.id = @curid
+FETCH NEXT FROM master_cursor INTO @curid END
+CLOSE master_cursor
+DEALLOCATE master_cursor
+end
+
+execute updatePSDcursor
+go
+
+select * from Master
+go
+
 --4) Хранимую процедуру доступа к метаданным
+create procedure cntrows as
+begin
+	SELECT o.name, i.rowcnt 
+	FROM sysindexes AS i INNER JOIN sys.tables AS o
+	ON i.id = o.object_id 
+	WHERE i.indid < 2  AND OBJECTPROPERTY(o.object_id, 'IsMSShipped') = 0
+	ORDER BY o.name
+end
+
+execute cntrows
+go
 
 --1) Триггер AFTER
+CREATE TRIGGER triggerAfterInsertDesigner
+ON Designer AFTER Insert
+AS 
+begin
+	declare @msg nvarchar(100)
+	set @msg = 'new designer ' +
+	(select first_name + ' ' + second_name
+	from Inserted) + ' is ready for work!'
+	print(@msg)
+end
+go
+
+drop trigger triggerAfterInsertDesigner
+go
+
+insert into designer(nick, first_name, second_name, email, gender, age)
+values('ivanov', 'Ivan', 'Ivanov', 'Ivan@gmail.com', 'M', 32)
+go
+--delete from Designer where nick = 'ivanov'
+
 --2) Триггер INSTEAD OF
+CREATE TRIGGER DenyInsert 
+ON Device
+INSTEAD OF INSERT
+AS
+BEGIN
+    RAISERROR('You have not permission to insert in this table.',10,1);
+END;
+
+insert into Device(code, size, ppi, name, nick)
+values ('jdjshdj', 2, 333, 'dsds', 'fddf')
+
+select * from Device
